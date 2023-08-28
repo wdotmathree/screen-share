5dbcbb48285a835910c35de279aedc13cd998786
wdotmathree <wdotmathree@gmail.com>
2023-08-28 17:24:06 -0600

    Add network implementation for linux

--- include/net.h
+++ include/net.h
@@ -4,4 +4,76 @@
 
 #include "include.h"
 
+#ifdef _WIN32
+#else
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#endif
+
+/**
+ * @brief Connect to a TCP server.
+ * @param host The hostname or IP address of the server
+ * @param port The port number of the server
+ * @return The sockfd of the connected socket, or -1 on error.
+ */
+int tcp_connect(const char *, int);
+
+/**
+ * @brief Send data to a TCP peer.
+ * @param sockfd The socket file descriptor
+ * @param buf The buffer to send
+ * @param len The length of the buffer
+ * @return Number of bytes sent, or -1 on error.
+ */
+int tcp_send(int, const char *, int);
+
+/**
+ * @brief Receive data from a TCP peer.
+ * @param sockfd The socket file descriptor
+ * @param buf The buffer to store the data
+ * @param len The length of the buffer
+ * @return Number of bytes received, or -1 on error.
+ */
+int tcp_recv(int, char *, int);
+
+/**
+ * @brief Close a socket.
+ * @param sockfd The socket file descriptor
+ */
+void sock_close(int);
+
+/**
+ * @brief Generate a sockaddr_in6 struct from a hostname and port.
+ * @return 0 on success, or -1 on error.
+ */
+int gen_addr(const char *, int, struct sockaddr_in6 *);
+
+/**
+ * @brief Create a UDP socket and bind it to a port.
+ * @param port The port number to bind to
+ * @return The sockfd of the socket, or -1 on error.
+ */
+int udp_bind(int);
+
+/**
+ * @brief Send data to a UDP peer.
+ * @param sockfd The socket file descriptor
+ * @param buf The buffer to send
+ * @param len The length of the buffer
+ * @param addr The address of the peer
+ * @return Number of bytes sent, or -1 on error.
+ */
+int udp_send(int, const char *, int, const struct sockaddr_in6 *);
+
+/**
+ * @brief Receive data from a UDP peer.
+ * @param sockfd The socket file descriptor
+ * @param buf The buffer to store the data
+ * @param len The length of the buffer
+ * @return Number of bytes received, or -1 on error.
+ */
+int udp_recv(int, char *, int);
+
 #endif
---
+++ linux/net.c
@@ -0,0 +1,100 @@
+#include "net.h"
+
+int tcp_connect(const char *host, int port) {
+	int sockfd = socket(AF_INET6, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		perror("socket");
+		return -1;
+	}
+	struct sockaddr_in6 addr;
+	addr.sin6_family = AF_INET6;
+	addr.sin6_port = htons(port);
+	struct addrinfo *res;
+	int ret = getaddrinfo(host, NULL, NULL, &res);
+	if (ret < 0) {
+		perror("getaddrinfo");
+		return -1;
+	}
+	memcpy(
+		&addr.sin6_addr, &((struct sockaddr_in6 *)res->ai_addr)->sin6_addr, sizeof(struct in6_addr)
+	);
+	if (connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		perror("connect");
+		return -1;
+	}
+	return sockfd;
+}
+
+int tcp_send(int sockfd, const char *buf, int len) {
+	int ret = send(sockfd, buf, len, 0);
+	if (ret < 0) {
+		perror("send");
+		return -1;
+	}
+	return ret;
+}
+
+int tcp_recv(int sockfd, char *buf, int len) {
+	int ret = recv(sockfd, buf, len, 0);
+	if (ret < 0) {
+		perror("recv");
+		return -1;
+	}
+	return ret;
+}
+
+void tcp_close(int sockfd) {
+	shutdown(sockfd, SHUT_RDWR);
+}
+
+int gen_addr(const char *host, int port, struct sockaddr_in6 *out) {
+	out->sin6_family = AF_INET6;
+	out->sin6_port = htons(port);
+	struct addrinfo *res;
+	int ret = getaddrinfo(host, NULL, NULL, &res);
+	if (ret < 0) {
+		perror("getaddrinfo");
+		return -1;
+	}
+	memcpy(
+		&out->sin6_addr, &((struct sockaddr_in6 *)res->ai_addr)->sin6_addr, sizeof(struct in6_addr)
+	);
+	return 0;
+}
+
+int udp_bind(int port) {
+	int sockfd = socket(AF_INET6, SOCK_DGRAM, 0);
+	if (sockfd < 0) {
+		perror("socket");
+		return -1;
+	}
+	struct sockaddr_in6 addr;
+	addr.sin6_family = AF_INET6;
+	addr.sin6_port = htons(port);
+	addr.sin6_addr = in6addr_any;
+	if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		perror("bind");
+		return -1;
+	}
+	return sockfd;
+}
+
+int udp_send(int sockfd, const char *buf, int len, const struct sockaddr_in6 *addr) {
+	int ret = sendto(sockfd, buf, len, 0, (struct sockaddr *)addr, sizeof(*addr));
+	if (ret < 0) {
+		perror("sendto");
+		return -1;
+	}
+	return ret;
+}
+
+int udp_recv(int sockfd, char *buf, int len) {
+	struct sockaddr_in6 addr;
+	socklen_t addrlen = sizeof(addr);
+	int ret = recvfrom(sockfd, buf, len, 0, (struct sockaddr *)&addr, &addrlen);
+	if (ret < 0) {
+		perror("recvfrom");
+		return -1;
+	}
+	return ret;
+}
